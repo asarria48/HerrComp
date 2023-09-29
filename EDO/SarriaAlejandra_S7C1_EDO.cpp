31adb1352fbfd859d58109855169d0d07c310cbd
asarria48 <asarria@unal.edu.co>
2023-09-28 21:49:57 -0500

    código con implementación de Euler y de Runge-Kutta

--- EDO/SarriaAlejandra_S7C1_EDO.cpp
+++ EDO/SarriaAlejandra_S7C1_EDO.cpp
@@ -16,6 +16,7 @@ int main(){
   double tf = 2.0;    //tiempo final
   double h = 0.01;
   int n = tf/h;
+  double k1, k2, k3, k4;    //inicializo las variables para el Runge-Kutta
   
   double y[n];    //arreglo de tamaño n donde guardaré mis valores de y
   double t[n];
@@ -23,16 +24,30 @@ int main(){
   t[0] = t0;
   y[0] = y0;
     
-  for(int i = 0; i < n ; i++){
+   for(int i = 0; i < n ; i++){
 
-    y[i+1] = y[i] + h*(f(y[i]));
-    t[i+1] = t[i] + h;
+     y[i+1] = y[i] + h*(f(y[i]));
+     t[i+1] = t[i] + h;
     
+     cout << t[i] << " " << y[i] << "\n";
+  }
+
+
+  for(int i = 0; i < n; i++){
+
+    k1 = h*f(y[i]);
+    k2 = h*f(y[i] + (k1/2));
+    k3 = h*f(y[i] + (k2/2));
+    k4 = h*f(y[i] + k3);
+
+    y[i+1] = y[i] + ((k1 + 2*k2 + 2*k3 + k4)/6);
+    t[i+1] = t[i] + h;
+
     cout << t[i] << " " << y[i] << "\n";
   }
 
   ofstream outFile("datosEuler.txt");
-  if (outFile.is_open()){
+  if(outFile.is_open()){
     for(int i = 0; i < n; i++){
       outFile << t[i] << " " << y[i] << "\n";
     }
