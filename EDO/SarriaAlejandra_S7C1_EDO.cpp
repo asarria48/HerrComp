8da0e3d368b7ef923513781c34bb299d8d444d1c
asarria48 <asarria@unal.edu.co>
2023-09-28 20:35:24 -0500

    Código correcto del método de Euler

---
+++ EDO/SarriaAlejandra_S7C1_EDO.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+double f(double y){
+
+  return -y;
+}
+
+int main(){
+
+  double y0 = 1.0;    //condición inicial
+  double t0 = 0.0;    //tiempo inicial
+  double tf = 2.0;    //tiempo final
+  double h = 0.01;
+  int n = tf/h;
+  
+  double y[n];    //arreglo de tamaño n donde guardaré mis valores de y
+  double t[n];
+
+  t[0] = t0;
+  y[0] = y0;
+    
+  for(int i = 0; i < n ; i++){
+
+    y[i+1] = y[i] + h*(f(y[i]));
+    t[i+1] = t[i] + h;
+    
+    cout << t[i] << " " << y[i] << "\n";
+  }
+
+  ofstream outFile("datosEuler.txt");
+  if (outFile.is_open()){
+    for(int i = 0; i < n; i++){
+      outFile << t[i] << " " << y[i] << "\n";
+    }
+
+    outFile.close();
+
+  }
+
+  return 0;
+}
