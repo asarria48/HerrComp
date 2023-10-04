059133d19fb6e11923e9560564c6f54f62af9e32
asarria48 <asarria@unal.edu.co>
2023-10-04 10:19:29 -0500

    primera prueba de Euler para EDO2 con datos para plotear en colab

---
+++ EDO2/SarriaAlejandra_S8CASA_EDO2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+const double k = 50.0;
+const double m = 0.2;
+
+double f(double x){
+
+  return -(k/m)*x;
+}
+
+int main(){
+
+  double x0 = 0.1;
+  double v0 = 0.0;
+  double tf = 2.0;
+  double h = 0.01;
+  int n = tf/h;
+
+  double x[n];
+  double v[n];
+
+  v[0] = v0;
+  x[0] = x0;
+
+  for(int i = 0; i < n - 1; i++){
+
+    v[i+1] = v[i] + h*f(x[i]);
+    x[i+1] = x[i] + h*v[i];
+
+    cout << v[i] << " "  << x[i] << "\n";
+  }
+
+  ofstream outfile;
+  outfile.open("euler2.dat");
+
+  outfile << v[0] << "," << x[0] << "\n";
+  for(int i = 0; i < n; i++){
+  outfile << v[i+1] << "," << x[i+1] << "\n";
+  }
+
+  outfile.close();
+
+  return 0; 
+}
