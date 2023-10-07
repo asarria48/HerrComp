7a97c5d3011706c9a396bf4b7af2a56e65a9bce8
asarria48 <asarria@unal.edu.co>
2023-10-06 20:44:44 -0500

    Primer intento de implementación de Runge Kutta para EDO2

--- EDO_2/SarriaAlejandra_resorteS8CASA_EDO2.cpp
+++ EDO_2/SarriaAlejandra_resorteS8CASA_EDO2.cpp
@@ -12,6 +12,11 @@ double f(double t, double x){
   return -k*x/m;
 }
 
+double s(double t){
+
+  return 0.1*cos(sqrt(k/m)*t);
+}
+
 int main(){
 
   double x0 = 0.1;
@@ -38,6 +43,10 @@ int main(){
 
   }
 
+
+  //cout << abs((f(t[0],x[0]) - s(t[0]))/s(t[0])) << endl;
+
+
   ofstream outfile;
   outfile.open("euler_2.dat");
 
@@ -49,6 +58,8 @@ int main(){
   outfile.close();
   
 
+  v[0] = v0 + 0.5*h*f(t[0],x[0]);
+  
   for(int i = 0; i < n; i++){
 
     x[i+1] = x[i] + h*v[i];
@@ -56,6 +67,8 @@ int main(){
     t[i+1] = t[i] + h;
 
   }
+
+  //cout << abs((f(t[0],x[0]) - s(t[0]))/s(t[0])) << endl;
 
     ofstream outFile;
   outFile.open("leapfrog_2.dat");
@@ -66,6 +79,40 @@ int main(){
   }
 
   outFile.close();
+
+  
+  double k1x, k2x, k3x, k4x;
+  double k1v, k2v, k3v, k4v;
+  
+  t[0] = t0;
+  x[0] = x0;
+  v[0] = v0;
+
+  for(int i = 0; i < n; i++){
+
+    k1x = f(t[i],x[i]);
+    k1v = f(t[i],v[i]);
+    k2x = f(t[i] + h/2 , x[i] + (h/2)*k1x);
+    k2v = f(t[i] + h/2 , v[i] + (h/2)*k1v);
+    k3x = f(t[i] + h/2 , x[i] + (h/2)*k2x);
+    k3v = f(t[i] + h/2 , v[i] + (h/2)*k2v);
+    k4x = f(t[i] + h , x[i] + h*k3x);
+    k4v = f(t[i] + h , v[i] + h*k3v);
+
+    x[i+1] = x[i] + h*(k1x + 2*k2x + 2*k3x + k4x)/6;
+    v[i+1] = v[i] + h*(k1v + 2*k2v + 2*k3v + k4v)/6;
+    t[i+1] = t[i] + h;
+  }
+
+   ofstream Outfile;
+  Outfile.open("rungekutta_2.dat");
+
+  Outfile << t[0] << " , " << x[0] << endl;
+  for(int i = 0; i < n; i++){
+    Outfile << t[i+1] << " , " << x[i+1] << endl;
+  }
+
+  Outfile.close();
 
   return 0;
 }
