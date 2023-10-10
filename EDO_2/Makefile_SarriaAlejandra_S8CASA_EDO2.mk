e4b258580e99235db2d4e60d6a58002ef7999e14
asarria48 <asarria@unal.edu.co>
2023-10-09 20:29:29 -0500

    intento de makefile

--- EDO_2/Makefile_SarriaAlejandra_S8CASA_EDO2.mk
+++ EDO_2/Makefile_SarriaAlejandra_S8CASA_EDO2.mk
@@ -1,16 +1,21 @@
-all: pdf
 
-pdf: resultados.tex euler leapfrog rungekutta
-	latex resultados.tex
+pdf: SarriaAlejandra_RESULTADOS_S8CASA_EDO2.tex euler leapfrog rungekutta eulerorbita leapfrogorbita
+	latex SarriaAlejandra_RESULTADOS_S8CASA_EDO2.tex
 
 euler: euler_2.dat plots_sarriaalejandra_resortes8casa_edo2.py
 	python plots_sarriaalejandra_resortes8casa_edo2.py
 
-leapfrog: leapfrog_2.dat plots_sarriaalejandra_resortes8casa_edo2.py
-	python plots_sarriaalejandra_resortes8casa_edo2.py
+leapfrog: leapfrog_2.dat PLOTS_SarriaAlejandra_resorteS8CASA_EDO2.py
+	python PLOTS_SarriaAlejandra_resorteS8CASA_EDO2.py
 
-rungekutta: rungekutta_2.dat plots_sarriaalejandra_resortes8casa_edo2.py
-	python plots_sarriaalejandra_resortes8casa_edo2.py
+rungekutta: rungekutta_2.dat PLOTS_SarriaAlejandra_resorteS8CASA_EDO2.py
+	python PLOTS_SarriaAlejandra_resorteS8CASA_EDO2.py
+
+eulerorbita: EulerPlanetas.dat PLOTS_SarriaAlejandra_S8planetasCASA_EDO2.py
+	python PLOTS_SarriaAlejandra_S8planetasCASA_EDO2.py
+
+leapfrogorbita: LeapfrogPlanetas.dat PLOTS_SarriaAlejandra_S8planetasCASA_EDO2.py
+	python PLOTS_SarriaAlejandra_S8planetasCASA_EDO2.py
 
 euler_2.dat: SarriaAlejandra_resorteS8CASA_EDO2.cpp
 	g++ SarriaAlejandra_resorteS8CASA_EDO2.cpp
