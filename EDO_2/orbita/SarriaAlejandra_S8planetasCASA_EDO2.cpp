76f8a5761738232eb8d19de59e763970db53eda9
asarria48 <asarria@unal.edu.co>
2023-10-09 19:31:34 -0500

    Código con Leapfrog y su .dat

--- EDO_2/orbita/SarriaAlejandra_S8planetasCASA_EDO2.cpp
+++ EDO_2/orbita/SarriaAlejandra_S8planetasCASA_EDO2.cpp
@@ -17,7 +17,7 @@ const double m = 6.68459e-12;       //Metros en una unidad astronómica (distanc
 
 const double s = 3.17098e-8;        //Segundos en un año
 
-const double G = (6.67428e-11)*m*m*m/(kg*s*s);    //Constante gravitacional (m^3/kg*s^2)
+const double G = (6.67428e-11)*m*m*m/(kg*s*s);    //Constante gravitacional (m^3/kg*s^2) (pasándolo a las unidades que estoy usando)
 
 double fx(double t, double x, double y){    //Función para resolver la ecuación de la órbita en x. Mi compañero Juan Diego Parales me ayudó a llegar a las ecuaciones 
                                             //que debo resolver
@@ -32,16 +32,16 @@ double fy(double t, double x, double y){    //Función para resolver la ecuació
 
 int main(){
   
-  double x0 = 0.9833;           //Distancia entre la Tierra y el Sol en el perihelio
+  double x0 = 0.983;                   //Distancia entre la Tierra y el Sol en el perihelio
   double y0 = 0.0;           
     
   double vx0 = 0.0;
-  double vy0 = 30.3*1000*m/s;           //Velocidad de la Tierra en el perihelio
+  double vy0 = 30750*m/s;              //Velocidad de la Tierra en el perihelio (pasándolo a las unidades que estoy usando)
 
   double t0 = 0.0;
-  double tf = 5.0;            //Un año en segundos
+  double tf = 1.0;                     //Tiempo de un año
   
-  double h = 0.0001;
+  double h = 0.001;
   
   int n = tf/h;
 
@@ -50,6 +50,8 @@ int main(){
   double vx[n];
   double vy[n];
   double t[n];
+
+  // Método de Euler:
 
   x[0] = x0;
   y[0] = y0;
@@ -86,5 +88,41 @@ int main(){
   outfile.close();
 
 
+  // Método de Leapfrog:
+
+  x[0] = x0;
+  y[0] = y0;
+
+  vx[0] = vx0 + (h/2)*fx(t[0], x[0], y[0]);    //Inicialización de leapfrog para vx
+  vy[0] = vy0 + (h/2)*fy(t[0], x[0], y[0]);    //Inicialización de leapfrog para vy
+
+  t[0] = t0;
+
+  for(int i = 0; i < n; i++){
+
+    x[i+1] = x[i] + h*vx[i];
+    y[i+1] = y[i] + h*vy[i];
+
+    vx[i+1] = vx[i] + fx(t[i+1], x[i+1], y[i+1]);
+    vy[i+1] = vy[i] + fy(t[i+1], x[i+1], y[i+1]);
+
+    t[i+1] = t[i] + h;
+    
+  }
+
+  ofstream Outfile;
+  outfile.open("LeapfrogPlanetas.dat");
+
+  outfile << t[0] << " , " << x[0] << " , " << y[0] << endl;
+
+  for(int i = 0; i < n; i++){
+
+    Outfile << t[i+1] << " , " << x[i+1] << " , " << y[i+1] << endl;
+  }
+  
+
+  Outfile.close();
+
+  
   return 0;
 }
